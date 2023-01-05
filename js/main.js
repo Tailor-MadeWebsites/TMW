191a6a7f902d346f647ee55d64b03f4fa2720e52
Tailor-MadeWebsites <77554212+Tailor-MadeWebsites@users.noreply.github.com>
2023-01-05 20:32:55 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -187,4 +187,31 @@ for (let i = 0, len = accordions.length; i < len; i++) { initAccordion(accordion
       }
     };
     xhr.send(data);
+
+
+// Contact
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+
+
+
+
   }
